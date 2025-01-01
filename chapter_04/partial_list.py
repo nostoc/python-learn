daba994171325f5a56e02b049bcbe79b2b41168d
nostoc <nostochk@gmail.com>
2025-01-01 23:16:02 +0530

    lists

---
+++ chapter_04/partial_list.py
@@ -0,0 +1,9 @@
+my_letters = ["a","b","c","d","e","f","g","h","i"]
+freind_letters = my_letters[:]
+print(my_letters)
+print(freind_letters)
+my_letters.append("Z")
+freind_letters.append("WW")
+
+print(my_letters)
+print(freind_letters)
