c6284029af4cd6c8ad182e98dcff90834e54c38c
nostoc <nostochk@gmail.com>
2025-01-15 22:34:43 +0530

    list with dictionaries

--- chapter_07/whi,le_with_dictionaries.py
+++ chapter_07/whi,le_with_dictionaries.py
@@ -1,11 +1,35 @@
-uncornfirmed_users = ["ashani","janani","hansika"]
-confirmed_users = []
-while uncornfirmed_users:
-    current_user = uncornfirmed_users.pop()
-    print(f"Verifying user : {current_user.title()}")
-    confirmed_users.append(current_user)
+# uncornfirmed_users = ["ashani","janani","hansika"]
+# confirmed_users = []
+# while uncornfirmed_users:
+#     current_user = uncornfirmed_users.pop()
+#     print(f"Verifying user : {current_user.title()}")
+#     confirmed_users.append(current_user)
 
-print("\nList of verified users\n")
-for user in confirmed_users:
-    print(user.title())
-print(uncornfirmed_users)
+# print("\nList of verified users\n")
+# for user in confirmed_users:
+#     print(user.title())
+# print(uncornfirmed_users)
+
+# pets =["cat","dog","parrot","fish","cat","duck","cat"]
+
+# while 'cat' in pets:
+#     pets.remove('cat')
+
+# for pet in pets:
+#     print(pet)
+
+reponses = {}
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat is your name ? ")
+    reponse = input("\nWhich mountain would you like to climb someday ? ")
+    reponses[name] = reponse
+    repeat = input("Would you like to let another person repond ? (yes/no) \n\t:")
+    if repeat == 'no':
+        polling_active = False
+
+print("\n-------POLLING RESULTS-------")
+for name, reponse in reponses.items():
+    print(f"{name.title() } would like to climb {reponse.title()} mountain one day.")
+print("\n")
