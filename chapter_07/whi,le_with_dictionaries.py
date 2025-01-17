e1414b52848b8e2952aedb0a1a3d761db69e8472
nostoc <nostochk@gmail.com>
2025-01-17 08:07:30 +0530

    change the activity

--- chapter_07/whi,le_with_dictionaries.py
+++ chapter_07/whi,le_with_dictionaries.py
@@ -19,18 +19,17 @@
 #     print(pet)
 
 reponses = {}
-polling_active = True # flag
+polling_active = True  # flag
 
 while polling_active:
     name = input("\nWhat is your name ? ")
-    reponse = input("\nWhich mountain would you like to climb someday ? ")
+    reponse = input("\nWhich country do you like to visit someday ? ")
     reponses[name] = reponse
     repeat = input("Would you like to let another person repond ? (yes/no) \n\t:")
-    if repeat == 'no':
+    if repeat == "no":
         polling_active = False
 
 print("\n-------POLLING RESULTS-------")
 for name, reponse in reponses.items():
-    print(f"{name.title() } would like to climb {reponse.title()} mountain one day.")
+    print(f"{name.title() } would like to visit {reponse.title()} one day.")
 print("\n")
-
