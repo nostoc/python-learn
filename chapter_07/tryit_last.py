8faff4e32e97a578c4401a0f45b01d1e3624638a
nostoc <nostochk@gmail.com>
2025-01-20 21:24:55 +0530

    task 2

--- chapter_07/tryit_last.py
+++ chapter_07/tryit_last.py
@@ -1,12 +1,27 @@
-sandwich_orders =[  'tuna', 'chicken', 'pastrami', 'beef']
+sandwich_orders =[  'pastrami','tuna','pastrami', 'chicken', 'pastrami', 'beef','pastrami',]
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
 finished_sandwiches=[]
 
 while sandwich_orders:
     for sandwich in sandwich_orders:
+        if sandwich=='pastrami':
+            sandwich_orders.remove(sandwich)     
         print(f"I made your {sandwich} sandwich.")
         finished_sandwiches.append(sandwich)
         sandwich_orders.remove(sandwich)
     
 print("\n----List of Finished Sandwiches----\n")
+print(f"\nDeli has run out of pastrami\n")
 for sandwich in finished_sandwiches:
     print(f"{sandwich} sandwich\n")
+
+# #2
+# while 'pastrami' in sandwich_orders:
+#     sandwich_orders.remove('pastrami')
+
+# print(f"\nDeli has run out of pastrami\n")
+# print("----List of availble sandwiches----\n")
+# for sandwich in sandwich_orders:
+#     print(f"{sandwich}")
