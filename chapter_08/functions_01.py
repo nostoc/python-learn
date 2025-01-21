4fe49e05730b3ed5da3e962630fe1462cda501d8
nostoc <nostochk@gmail.com>
2025-01-21 23:33:39 +0530

    function -2

--- chapter_08/functions_01.py
+++ chapter_08/functions_01.py
@@ -1,5 +1,15 @@
-def gree_user():
+def gree_user(username):
     """Display a simple greeting"""
-    print("Hello")
+    print(f"Hello {username.title()}")
 
-gree_user()
+gree_user("hansika")
+
+def display_message(msg):
+    print(f"{msg}")
+
+display_message("I am learning about python functions is this chapter")
+
+def favorite_book(title):
+    print(f"My favorite book is {title.title()}")
+
+favorite_book("Harry Potter Series")
