d0edbbb18e8424f3803ac078b8dbe11fda6ad0ae
nostoc <nostochk@gmail.com>
2025-01-23 23:13:47 +0530

    functions

--- chapter_08/functions_01.py
+++ chapter_08/functions_01.py
@@ -13,3 +13,20 @@ def favorite_book(title):
     print(f"My favorite book is {title.title()}")
 
 favorite_book("Harry Potter Series")
+
+def get_formatted_name(first_name,last_name,middle_name=''):
+    if middle_name:
+        print(f"{first_name} {middle_name} {last_name}")
+    else:
+        print(f"{first_name} {last_name}")
+
+get_formatted_name("Hansika","Karunathilake")
+get_formatted_name("Janitha","Karunarathna","Senani")
+
+def build_person(first_name,last_name):
+    person = { 'first' : first_name, 'last': last_name}
+    return person
+
+musician = build_person("John","Parker")
+print(musician)
+
