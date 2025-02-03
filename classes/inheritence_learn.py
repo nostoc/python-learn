13c68e89e47da4f870ad35a798971ad7a941e418
nostoc <nostochk@gmail.com>
2025-02-04 00:19:22 +0530

    composition

--- classes/inheritence_learn.py
+++ classes/inheritence_learn.py
@@ -20,26 +20,36 @@ class Car:
 
     def increament_odometer(self, increament):
         self.odometer_reading += increament
-    
+
     def fill_gas_tank(self, tank_amount):
         self.tank_amount = tank_amount
         print(f"Gas tank is {self.tank_amount} amount filled.")
 
 
+class Battery():
+    """Model of a battery for an electric car"""
+
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(
+            f"This car has a battery of size, {self.battery_size} kWh."
+        )
+
+
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-        self.battery_size = 40
-        
-    def describe_battery(self):
-        print(f"{self.get_descriptive_name()} has a battery of size, {self.battery_size}")  
-    
+        self.battery = Battery()
+
     def fill_gas_tank(self):
         """Electric cars don't have gas tanks"""
-        print("This car does not have a gas tank!") 
-        
-my_leaf = ElectricCar("nisam","leaf",2024)
-print(my_leaf.get_descriptive_name())
-print(my_leaf.describe_battery())
-print(my_leaf.fill_gas_tank())
+        print("This car does not have a gas tank!")
 
+
+my_leaf = ElectricCar("nisam", "leaf", 2024)
+print(my_leaf.get_descriptive_name())
+# print(my_leaf.describe_battery())
+print(my_leaf.fill_gas_tank())
+my_leaf.battery.describe_battery()
