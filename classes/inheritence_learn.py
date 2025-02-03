da106addbf05bae20746b99ca30706507f2e0a0f
nostoc <nostochk@gmail.com>
2025-02-04 00:40:58 +0530

    composition 2.0

--- classes/inheritence_learn.py
+++ classes/inheritence_learn.py
@@ -26,23 +26,28 @@ class Car:
         print(f"Gas tank is {self.tank_amount} amount filled.")
 
 
-class Battery():
+class Battery:
     """Model of a battery for an electric car"""
 
     def __init__(self, battery_size=40):
         self.battery_size = battery_size
 
     def describe_battery(self):
-        print(
-            f"This car has a battery of size, {self.battery_size} kWh."
-        )
+        print(f"This car has a battery of size, {self.battery_size} kWh.")
+
+    def get_range(self):
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 45:
+            range = 225
+        print(f"The range of this car is {range} km.")
 
 
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
         self.battery = Battery()
-
+ 
     def fill_gas_tank(self):
         """Electric cars don't have gas tanks"""
         print("This car does not have a gas tank!")
@@ -53,3 +58,4 @@ print(my_leaf.get_descriptive_name())
 # print(my_leaf.describe_battery())
 print(my_leaf.fill_gas_tank())
 my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
