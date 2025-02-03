3c6a82d1f0796afea15007e2162c6c37cf1f76a2
nostoc <nostochk@gmail.com>
2025-02-03 23:59:43 +0530

    inheritence 2.0

--- classes/inheritence_learn.py
+++ classes/inheritence_learn.py
@@ -25,7 +25,13 @@ class Car:
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-
+        self.battery_size = 40
+        
+    def describe_battery(self):
+        print(f"{self.get_descriptive_name()} has a battery of size, {self.battery_size}")     
+        
 my_leaf = ElectricCar("nisam","leaf",2024)
 print(my_leaf.get_descriptive_name())
+print(my_leaf.describe_battery())
+
 
