33fe274d5adbc1104490dee18189e4946bf89c71
nostoc <nostochk@gmail.com>
2025-02-04 20:50:50 +0530

    read from a file 2.0

--- files-and-exceptions/file_reader.py
+++ files-and-exceptions/file_reader.py
@@ -1,5 +1,20 @@
 from pathlib import Path
 
-path = Path("pi_digits.txt")
+path = Path("pi_million.txt")
 contents = path.read_text().rstrip()
-print(contents)
+
+lines = contents.splitlines()
+
+pi_string = ""
+
+for line in lines:
+    pi_string = pi_string + line.strip()
+
+# print(f"{pi_string[:52]}")
+# print(len(pi_string))
+
+birthday=input("ENter your birthday in the form mmddyyyy : ")
+if birthday in pi_string:
+    print(f"Your birthday appears in first {len(pi_string)} digits of pi")
+else:
+    print(f"Your birthday doesnot appear in first {len(pi_string)} digits of pi")
