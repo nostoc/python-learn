cf7c21a88d83bfa917b886c852005dc41423c76d
nostoc <nostochk@gmail.com>
2025-02-05 18:04:55 +0530

    program to handle exception

--- files-and-exceptions/exceptions/division_calculator.py
+++ files-and-exceptions/exceptions/division_calculator.py
@@ -1,4 +1,19 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't divide by zero !")
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You can't divide by zero !")
+
+
+print("Give me two numbers and I'll divide them")
+print("Enter 'q' to exit.")
+
+while True:
+    first_number = input("\nFirst Number: ")
+    if first_number.lower() == "q":
+        break
+    second_number = input("\nSecond Number: ")
+    if second_number.lower() == "q":
+        break
+    answer = int(first_number)/int(second_number)
+    print(f"Your answer is : {answer}")
+    
