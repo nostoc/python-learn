ffb353a4b3e12b0746b6221f4749ff5692e43422
nostoc <nostochk@gmail.com>
2025-02-05 19:34:10 +0530

    analyse the text file

--- chapter_10/exceptions/alice.py
+++ chapter_10/exceptions/alice.py
@@ -5,3 +5,9 @@ try:
     contents = path.read_text(encoding='utf-8')
 except FileNotFoundError:
     print("Sorry the file 'alice.txt' does not exist")
+else:
+    #count the number of words in the file
+    words  = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")
+    #print(words)
