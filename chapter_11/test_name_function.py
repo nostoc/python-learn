71d29605ceea4bb9c3e61f5b18bd6c5e431f9a28
nostoc <nostochk@gmail.com>
2025-02-05 23:43:45 +0530

    test

--- chapter_11/test_name_function.py
+++ chapter_11/test_name_function.py
@@ -4,5 +4,8 @@ from name_function import get_formatted_name
 def test_first_name_last_name():
     """Do names like "Janis Joplin" work?"""
     
-    formatted_name = get_formatted_name("Janis","Joplin")
-    assert formatted_name == "Janis Joplin"
+    formatted_name1 = get_formatted_name("Janis","Joplin")
+    assert formatted_name1 == "Janis Joplin"
+    
+   
+
