7c7fc1d5de3e1720f5047935a5e095664f993c1f
nostoc <nostochk@gmail.com>
2025-02-05 17:25:17 +0530

    writing multiple lines

--- files-and-exceptions/write_message.py
+++ files-and-exceptions/write_message.py
@@ -1,4 +1,9 @@
 from pathlib import Path
 
+contents = "I lve cats\n"
+contents += 'I love programming\n'
+contents += 'I love Machine Learning'
+
+
 path = Path("files-and-exceptions/programming.txt")
-path.write_text("I love programming")
+path.write_text(contents)
