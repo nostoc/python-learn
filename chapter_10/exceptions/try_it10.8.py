d554012886602bb58956d0505933a819a0d5f415
nostoc <nostochk@gmail.com>
2025-02-05 20:39:53 +0530

    fix a bug

--- chapter_10/exceptions/try_it10.8.py
+++ chapter_10/exceptions/try_it10.8.py
@@ -4,9 +4,13 @@ dogs_path = Path("chapter_10/exceptions/dogs.txt")
 cats_path = Path("chapter_10/exceptions/cats.txt")
 
 def read_file(path):
-    contents = dogs_path.read_text()
-    print(f"COntents of the file {path}")
-    print(contents)
+    try:
+        contents = path.read_text()
+    except FileNotFoundError:
+        print(f"Oops the {path} file doesnot exist.")
+    else:
+        print(f"Contents of the file {path}")
+        print(contents)
 
 read_file(cats_path)
 read_file(dogs_path)
