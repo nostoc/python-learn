8ea4eebf33e787cef37f4ffad3346c3fd42991f7
nostoc <nostochk@gmail.com>
2025-02-05 19:40:06 +0530

    create a function to count_words

--- chapter_10/exceptions/alice.py
+++ chapter_10/exceptions/alice.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 
 path = Path("chapter_10/exceptions/alice.txt")
-try:
-    contents = path.read_text(encoding='utf-8')
-except FileNotFoundError:
-    print("Sorry the file 'alice.txt' does not exist")
-else:
-    #count the number of words in the file
-    words  = contents.split()
-    num_words = len(words)
-    print(f"The file {path} has about {num_words} words.")
-    #print(words)
+
+def count_words(path):
+    """count the approximate number of words in the file"""
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Sorry the {path} file  does not exist.")
+    else:
+
+        words  = contents.split()
+        num_words = len(words)
+        print(f"The file {path} has about {num_words} words.")
+        # print(words)
+
