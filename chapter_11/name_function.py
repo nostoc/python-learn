cd11ae922cf3cddb2976e92607e4f2e16a74d716
nostoc <nostochk@gmail.com>
2025-02-05 23:27:56 +0530

    failing test

--- chapter_11/name_function.py
+++ chapter_11/name_function.py
@@ -1,6 +1,6 @@
-def get_formatted_name(first,last):
+def get_formatted_name(first,middle,last):
     '''Generate a neatly formatted full name'''
-    full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
 
 
