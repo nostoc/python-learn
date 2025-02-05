7b26c9a94225ee5d53f3aaf9c19422d2ee51f0e8
nostoc <nostochk@gmail.com>
2025-02-05 21:45:06 +0530

    refactorung

--- chapter_10/store_data/remeber_me.py
+++ chapter_10/store_data/remeber_me.py
@@ -1,14 +1,23 @@
 from pathlib import Path
 import json
 
+def get_stored_name(path):
+    '''Get user name if available'''
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+    
+
 
 def greet_user():
     """Greet the user by name"""
     path = Path("chapter_10/store_data/username.json")
-    if path.exists():
-        contents = path.read_text()
-        username = json.loads(contents)
-        print(f"Welcome Back {username}!")
+    username = get_stored_name(path)
+    if username:
+        print(f"Welcome back {username}!")
     else:
         username = input("What is your user name? ")
         contents = json.dumps(username)
