d4de20e961457b5c7348a6bebf85366d152b7061
nostoc <nostochk@gmail.com>
2025-02-05 17:49:26 +0530

    write multiple lines

--- files-and-exceptions/try_it10.4.py
+++ files-and-exceptions/try_it10.4.py
@@ -1,5 +1,22 @@
 from pathlib import Path
 
-path = Path("files-and-exceptions/guests.txt")
-name = input("What is your name ?")
-path.write_text(name)
+# path = Path("files-and-exceptions/guests.txt")
+path = Path("files-and-exceptions/guest_book.txt")
+
+# path.write_text(name)
+
+guest_book =[]
+content= ''
+
+
+while True:
+    name = input("Enter your name ? (enter 'q' to exit.)")
+    
+    if name =='q':
+        break
+    guest_book.append(name)
+
+for guest in guest_book:
+    content += f'{guest}\n'
+
+path.write_text(content)
