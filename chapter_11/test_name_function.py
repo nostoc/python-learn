cd2f460fb2720f6b0057a9e35fe4d016351dc418
nostoc <nostochk@gmail.com>
2025-02-05 23:46:32 +0530

    test 2

--- chapter_11/test_name_function.py
+++ chapter_11/test_name_function.py
@@ -3,9 +3,13 @@ from name_function import get_formatted_name
 
 def test_first_name_last_name():
     """Do names like "Janis Joplin" work?"""
-    
-    formatted_name1 = get_formatted_name("Janis","Joplin")
-    assert formatted_name1 == "Janis Joplin"
-    
-   
+
+    formatted_name = get_formatted_name("Janis", "Joplin")
+    assert formatted_name == "Janis Joplin"
+
+
+def test_first_name_middle_name_last_name():
+    """Do names like Janitha senani karunarathna work?"""
+    formatted_name = get_formatted_name("Janitha", "Karunarathna", "Senani")
+    assert formatted_name == "Janitha Senani Karunarathna"
 
