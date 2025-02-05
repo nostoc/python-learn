27a68d2e9d3e629cc2879d2c98d837181c754199
nostoc <nostochk@gmail.com>
2025-02-06 00:08:01 +0530

    add a new test

--- chapter_11/try_it/test_cities.py
+++ chapter_11/try_it/test_cities.py
@@ -3,3 +3,8 @@ from city_functions import get_location_name
 def test_city_country():
     formatted_location=get_location_name("Gampha","Sri Lanka")
     assert formatted_location == "Gampha, Sri Lanka"
+
+
+def test_city_country_population():
+    formatted_location = get_location_name("Gampha", "Sri Lanka",2000000)
+    assert formatted_location == "Gampha, Sri Lanka - Population 2000000"
